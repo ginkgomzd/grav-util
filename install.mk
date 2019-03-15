4fa307833e1a15bdf0a0ca3999195c9bc934cde7
ginkgoMZD <mzd@ginkgostreet.com>
2019-03-15 13:37:10 -0400

    install.mk

---
+++ install.mk
@@ -0,0 +1,15 @@
+install: uninstall /usr/local/bin/grav-util
+
+uninstall:
+	- sudo rm -rf /usr/local/lib/grav-util
+	- sudo rm /usr/local/bin/grav-util
+
+/usr/local/lib/grav-util:
+	sudo cp -a ./ ${@}
+
+/usr/local/bin/grav-util: /usr/local/lib/grav-util
+	sudo ln -s /usr/local/lib/grav-util/grav-util ${@}
+
+/etc/bash_completion.d/grav-util:
+	sudo cp ./completions /etc/bash_completion.d/grav-util
+
