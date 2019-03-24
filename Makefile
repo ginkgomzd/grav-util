4dfebd1b39bfc68569d405204923429ee507e518
ginkgoMZD <mzd@ginkgostreet.com>
2019-03-24 00:00:45 -0400

    add serve-grav (stand-alone) command

--- Makefile
+++ Makefile
@@ -35,9 +35,5 @@ project-lab-repo: | require-env-PROJ_LAB_REPO require-env-PUBLIC_HTML
 dev-env: project-lab-repo
 	$(MAKE) -f ${THIS_DIR}/ginkgo-grav/Makefile dev-env admin-login ginkgo-rtfm
 
-serve:
-	cd '${PUBLIC_HTML}' && \
-	php -S localhost:8947 system/router.php
-
 new-doc:
 	$(MAKE) -f ${THIS_DIR}/new-feature/Makefile new-doc
--- install.mk
+++ install.mk
@@ -1,15 +1,19 @@
 
-install: uninstall /usr/local/bin/grav-util /etc/bash_completion.d/grav-util
+install: uninstall /usr/local/bin/grav-util /usr/local/bin/serve-grav /etc/bash_completion.d/grav-util
 
 uninstall:
 	- sudo rm -rf /usr/local/lib/grav-util
 	- sudo rm /usr/local/bin/grav-util
+	- sudo rm /etc/bash_completion.d/grav-util
 
 /usr/local/lib/grav-util:
 	sudo cp -a ./ ${@}
 
 /usr/local/bin/grav-util: /usr/local/lib/grav-util
 	sudo ln -s /usr/local/lib/grav-util/grav-util ${@}
+
+/usr/local/bin/serve-grav: /usr/local/lib/grav-util
+	sudo ln -s /usr/local/lib/grav-util/serve-grav ${@}
 
 /etc/bash_completion.d/grav-util:
 	sudo cp ./completions /etc/bash_completion.d/grav-util
